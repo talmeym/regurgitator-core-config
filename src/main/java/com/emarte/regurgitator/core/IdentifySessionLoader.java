8587deb57f93b441a6e7cba6681caeea7d4ee2e5
talmeym <mtalmey@gmail.com>
2016-03-11 16:06:26 +0000

    added value source param to identify-session

---
+++ src/main/java/com/emarte/regurgitator/core/IdentifySessionLoader.java
@@ -0,0 +1,17 @@
+package com.emarte.regurgitator.core;
+
+public abstract class IdentifySessionLoader {
+	protected Step buildIdentifySession(String id, String source, String value, Log log) throws RegurgitatorException {
+		int numberSet = 0;
+		numberSet = source != null ? ++numberSet : numberSet;
+		numberSet = value != null ? ++numberSet : numberSet;
+
+		if(numberSet == 0) {
+			throw new RegurgitatorException("Source or value is required");
+		}
+
+		ContextLocation location = source != null ? new ContextLocation(source) : null;
+		log.debug("Loaded identify session '" + id + "'");
+		return new IdentifySession(id, new ValueSource(location, value));
+	}
+}
