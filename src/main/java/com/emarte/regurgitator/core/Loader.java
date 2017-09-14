b99fcafa5f65933ebf92815a3046f0ba3e2d5afa
talmeym <mtalmey@gmail.com>
2017-09-14 13:28:46 +0100

    Refactored loaders and yml loading

--- src/main/java/com/emarte/regurgitator/core/Loader.java
+++ src/main/java/com/emarte/regurgitator/core/Loader.java
@@ -1,4 +1,7 @@
 package com.emarte.regurgitator.core;
 
-public interface Loader {
+import java.util.Set;
+
+public interface Loader<DATA, RESULT> {
+	RESULT load(DATA DATA, Set<Object> allIds) throws RegurgitatorException;
 }
