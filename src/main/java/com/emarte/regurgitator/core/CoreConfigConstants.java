150b345038865326a6b4f57a38bcc0ded233d40d
talmeym <mtalmey@gmail.com>
2019-05-23 08:38:30 +0100

    added list processor

--- src/main/java/com/emarte/regurgitator/core/CoreConfigConstants.java
+++ src/main/java/com/emarte/regurgitator/core/CoreConfigConstants.java
@@ -34,4 +34,6 @@ public interface CoreConfigConstants {
     String ISOLATE = "isolate";
     String AS_INDEX ="as-index";
     String FOLDER ="folder";
+    String INDEX_SOURCE = "index-from";
+    String VALUE_SOURCE = "value-from";
 }
